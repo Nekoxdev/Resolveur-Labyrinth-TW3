ec44a97cf8119f78c6d37e8fedde1b922886fb4c
I-vortex94 <131229765+I-vortex94@users.noreply.github.com>
2025-01-10 17:03:20 +0100

    Bordures

--- maze.py
+++ maze.py
@@ -10,7 +10,14 @@ class Laby:
         depart_x = random.randrange(1, self.largeur, 2) # 2 pour s'assurer qu'il ne tombe pas sur un bord
         depart_y = random.randrange(1, self.hauteur, 2) # 2 pour s'assurer qu'il ne tombe pas sur un bord
         self.labyrinthe[depart_x][depart_y] #Place le départ (D) sur le laby
-    
+
+        for i in range(self.largeur):
+            self.labyrinthe[0][i] = '#'
+            self.labyrinthe[self.hauteur - 1][i] = '#'
+        for i in range(self.largeur):
+            self.labyrinthe[i][0] = '#'
+            self.labyrinthe[i][self.largeur - 1] = '#'
+
     def afficher(self):
         # Affiche le laby
         for ligne in self.labyrinthe:
