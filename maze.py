dc36895006b444b836842912792d251a079b2457
I-vortex94 <131229765+I-vortex94@users.noreply.github.com>
2025-01-10 16:57:35 +0100

    Ajouter le départ 'D'

--- maze.py
+++ maze.py
@@ -1,18 +1,25 @@
 class Laby:
     def __init__(self, largeur, hauteur):
-        self.largeur = largeur
-        self.hauteur = hauteur
-        # Grille remplie de murs
+        self.largeur = largeur #largeur du laby
+        self.hauteur = hauteur #hauteur du laby
+        # Grille remplie de murs : sous forme de matrice
         self.labyrinthe = [['#' for _ in range(largeur)] for _ in range(hauteur)]
 
-  def afficher(self):
-    # Affiche le laby
-    for ligne in self.labyrinthe:
-      print(''.join(ligne))
+    def generer(self): #Pour faire le laby
+        #Placer le départ (D)
+        depart_x = random.randrange(1, self.largeur, 2) # 2 pour s'assurer qu'il ne tombe pas sur un bord
+        depart_y = random.randrange(1, self.hauteur, 2) # 2 pour s'assurer qu'il ne tombe pas sur un bord
+        self.labyrinthe[depart_x][depart_y] #Place le départ (D) sur le laby
+    
+    def afficher(self):
+        # Affiche le laby
+        for ligne in self.labyrinthe:
+          print(''.join(ligne))
 
 largeur = 31
 hauteur = 41
 # (Parce que ça fait 3141 : 3,141)
 labyrinthe = Laby(largeur, hauteur)
 
+labyrinth.generer()
 labyrinth.afficher()
